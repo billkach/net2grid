c5920a852c788f2d19014d4a1a0a9a18b059b067
billkach <billkach@gmail.com>
2021-09-22 11:23:37 -0700

    template for PS script

---
+++ net2grid.ps1
@@ -0,0 +1,18 @@
+# Script Name                           net2grid.ps1
+# Authors                               Kwesi Keller, Marcus Miles, Bill Kachersky
+# Date of last revision                 09/22/2021
+# Description of purpose                
+
+# Variables
+
+
+
+# Functions
+
+
+
+# Main
+
+
+
+# End
