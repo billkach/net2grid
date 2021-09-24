156649a7732ff2c7a7a2d8b8225ec84476d58277
Kwesi Keller <89751532+Kwesik@users.noreply.github.com>
2021-09-23 23:04:12 -0700

    Add files via upload

---
+++ NewOU1.ps1
@@ -0,0 +1,8 @@
+﻿# Create a Organizational Unit
+
+New-ADOrganizationalUnit -Name "Marketing"
+New-ADOrganizationalUnit -Name "Finance"
+New-ADOrganizationalUnit -Name "Human Resources"
+New-ADOrganizationalUnit -Name "IT"
+New-ADOrganizationalUnit -Name "ITSec"
+New-ADOrganizationalUnit -Name "Operations"
